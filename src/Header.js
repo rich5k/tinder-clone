d095262d09653960cc962af7addaf124f5108133
rich5k <richardanatsui@gmail.com>
2020-12-18 19:45:37 +0000

    added header

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+
+
+function Header() {
+    return (
+        <div className="header"> 
+           <IconButton>
+           <PersonIcon fontSize="large" className="header_icon" />
+
+           </IconButton>
+           <img 
+                className="header_logo"
+                src="https://www.globaldatinginsights.com/wp-content/uploads/2017/08/Screen-Shot-2017-08-17-at-10.04.531.png"
+                alt=""
+
+            />
+        </div>
+    )
+}
+
+export default Header
